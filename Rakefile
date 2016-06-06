f2dc5dddb2c824d7f54bed24faa7c03ef929dee6
Richard Davis <rdavis.bacs@gmail.com>
2016-06-06 17:14:18 -0400

    configure test suite

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require './app'
 require 'sinatra/activerecord/rake'
+
+begin
+  require 'rspec/core/rake_task'
+  RSpec::Core::RakeTask.new(:spec)
+rescue LoadError
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,11 @@ require_relative '../app.rb'
 set :environment, :test
 set :database, :test
 
+ActiveRecord::Base.logger.level = 1
+
 Capybara.app = Sinatra::Application
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
