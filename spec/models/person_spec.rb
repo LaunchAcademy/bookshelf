ebcf1fd297751c89c15bd3dbc229902c3166b007
Richard Davis <rdavis.bacs@gmail.com>
2016-06-06 17:16:58 -0400

    refactor tests using 'let'

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,20 +2,14 @@ require "spec_helper"
 
 RSpec.describe Person do
   describe "#name" do
-    it "allows you to set the name" do
-      person = Person.new
-      person.name = "Richard"
-      person.save
+    let(:person) { Person.create(name: "Richard") }
 
+    it "allows you to set the name" do
       expect(person).to be_a(Person)
       expect(person.persisted?).to eq(true)
     end
 
     it "allows you to get the name" do
-      person = Person.new
-      person.name = "Richard"
-      person.save
-
       expect(person.name).to eq("Richard")
     end
   end
